374164ff27a95d4591eefb29b9e36e06ecf72e40
satwikigurunath <76837776+satwikigurunath@users.noreply.github.com>
2021-12-26 15:40:54 +0530

    Add files via upload

---
+++ screens/StoryCard.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <TouchableOpacity
+          style={styles.container}
+          onPress={() =>
+            this.props.navigation.navigate("StoryScreen", {
+              story: this.props.story
+            })
+          }
+        >
+          <View style={styles.cardContainer}>
+            <Image
+              source={require("../assets/download.jpg")}
+              style={styles.storyImage}
+            ></Image>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.storyTitleText}>
+                {this.props.story.title}
+              </Text>
+              <Text style={styles.storyAuthorText}>
+                {this.props.story.author}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {this.props.story.description}
+              </Text>
+            </View>
+            <View style={styles.actionContainer}>
+              <View style={styles.likeButton}>
+                <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                <Text style={styles.likeText}>12k</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardContainer: {
+    margin: RFValue(30),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  storyImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250)
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center"
+  },
+  storyTitleText: {
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  storyAuthorText: {
+    fontSize: RFValue(20),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 15,
+    color: "white",
+    paddingTop: RFValue(10)
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(30)
+  },
+  likeButton: {
+    width: RFValue(130),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#fb4050",
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(30),
+    marginLeft: RFValue(5)
+  }
+});
